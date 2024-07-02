1ce9ab293bfd2e22a5d5037a639ea7372004a1f4
Matthew Tan <tanmatt1515@gmail.com>
2024-07-02 17:30:03 -0400

    added room participants

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -25,8 +25,8 @@ export async function POST(request: Request) {
     return new Response("Unauthorized", { status: 403 });
 
   const userInfo = {
-    name: user.firstName!,
-    picture: user.imageUrl!,
+    name: user.firstName || "Teammate",
+    picture: user.imageUrl,
   };
 
   const session = liveblocks.prepareSession(user.id, { userInfo });
